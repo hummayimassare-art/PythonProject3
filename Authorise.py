de405598883b28a69ede9e4f50c8c47220306c61
Hummay Issa <hummay.i.massare@gmail.com>
2025-11-05 13:56:34 +0400

    Initial commit

---
+++ Authorise.py
@@ -0,0 +1,13 @@
+import bcrypt
+def hash_password(plain_text_pass):
+    pass_bytes = plain_text_pass.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pass = bcrypt.hashpw(pass_bytes,salt)
+    return hashed_pass
+
+passwd = "apple"
+pass_hash = hash_password(passwd)
+print(f'password: {passwd} Hash: {str(pass_hash)}')
+
+passwd = "apple"
+pass hash + hash
